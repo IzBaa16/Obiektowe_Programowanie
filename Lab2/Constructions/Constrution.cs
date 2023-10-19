4d907e7824f9aaf6d06be1ecdf6494289bd6cfac
Jakub Izdebski <w67235@student.wsiz.edu.pl>
2023-10-19 15:49:27 +0200

    lab2

---
+++ Lab2/Constructions/Constrution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Constructions
+{
+    class Construction
+    { 
+        //zad4
+        public Construction(double height, double width, int entrances, 
+            int humanCapacity, string buildMaterial)
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            BuildMaterial = buildMaterial;
+        }
+
+        //zad3
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { get; set; }
+
+
+    }
+}
+
+
+
---
+++ Lab2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Lab2.Constructions;
+using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var construction = new Construction(23, 45, 4, 6, "Brik");
+            Console.WriteLine(construction.BuildMaterial);
+        }
+    }
+}
